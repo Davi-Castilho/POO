213d161f9f09ef7be54450d848cfc88379f48587
Davi-Castilho <davicast04@gmail.com>
2025-04-07 12:34:48 -0300

    Add files via upload

---
+++ PYTHON.py
@@ -0,0 +1,44 @@
+print("Olá mundo")
+print("Hello world")
+a = 'I\'m ok'
+a = "I'm ok"
+print(a)
+
+x=10
+print(x)
+fruta = "pera" 
+print(f"banana {fruta} maça") #f é pra formatar, \n pra pular linha e \t para dar espaço
+print(type (9)) # type retorna o tipo da classe
+print(type (10.0))
+print(type ("ola"))
+print(type (False))
+# == igualdade   
+# = atribuição 
+
+x = 25
+y = 10
+
+print(x + y) # + -> soma
+print(x - y) # - -> subtração
+print(x * y) # * -> multiplicação
+print(x / y) # / -> divisão
+
+print(x % y) # % -> resto
+print(x ** y) # ** -> potenciação
+print(x // y) # retorna  o menor numero inteiro
+x+=y
+print(x)
+
+a = 'banana'
+b = 'maça'
+print(a + b)
+print(a * 2)
+print(a+' '+b)
+
+x = input("Qual o seu nome?: ")
+a = int(input("Digite um número: "))
+
+x = 20
+y = 5
+print(x >= y)
+print(x != y)
